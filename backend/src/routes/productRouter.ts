5179bec8b0973f13dc4864bf794e8fc2943b2f90
martin <martinberon02@gmail.com>
2025-06-29 16:10:06 -0300

    Agregar ruta de búsqueda por nombre en productRouter

--- backend/src/routes/productRouter.ts
+++ backend/src/routes/productRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { createProduct, deleteProduct, getAllProducts, updateProduct } from "../controllers/productControllers"
+import { createProduct, deleteProduct, getAllProducts, updateProduct, searchProductsByName } from "../controllers/productControllers"
 import { authMiddleware } from "../middleware/authMiddleware"
 
 const productRouter = Router()
@@ -8,5 +8,7 @@ productRouter.get("/", getAllProducts)
 productRouter.post("/", authMiddleware, createProduct)
 productRouter.delete("/:id", authMiddleware, deleteProduct)
 productRouter.patch("/:id", authMiddleware, updateProduct)
+
+productRouter.get("/search", searchProductsByName); // Nueva ruta para búsqueda
 
 export { productRouter }
